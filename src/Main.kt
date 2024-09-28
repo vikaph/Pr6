5de518cf2da5d177b133dac4a0731f2dc363e86d
357-22 <vika.p.2006@bk.ru>
2024-09-28 09:41:30 +0300

    Готовое решение

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    println("Введите первую цифру (0-9):")
+    val firstInput = readLine()
+    val firstDigit = firstInput?.toIntOrNull()
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    println("Введите вторую цифру (0-9):")
+    val secondInput = readLine()
+    val secondDigit = secondInput?.toIntOrNull()
+
+    if (firstDigit == null || secondDigit == null ||
+        firstDigit < 0 || firstDigit > 9 ||
+        secondDigit < 0 || secondDigit > 9 ||
+        firstDigit == secondDigit) {
+        println("Ввод некорректен. Цифры должны находиться в диапазоне от 0 до 9 и быть различными")
+        return
+    }
+    var oddNumber: Int? = null
+    if (firstDigit % 2 != 0) {
+        oddNumber = firstDigit * 10 + secondDigit
+    } else if (secondDigit % 2 != 0) {
+        oddNumber = secondDigit * 10 + firstDigit
+    }
+    if (oddNumber != null) {
+        println("Созданное нечетное число: $oddNumber")
+    } else {
+        println("Создать нечетное число невозможно.")
     }
 }
